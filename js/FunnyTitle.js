88f54aa66d66bf9ad6103ecdc21042e75cc0e453
jnyou <1447177020@qq.com>
2020-06-20 21:13:57 +0800

    Site updated: 2020-06-20 21:13:47

---
+++ js/FunnyTitle.js
@@ -0,0 +1,3 @@
+// build time:Sat Jun 20 2020 21:13:01 GMT+0800 (GMT+08:00)
+var OriginTitile=document.title,st;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="ヽ(●-`Д´-)ノ你要玩捉迷藏嘛",clearTimeout(st)):(document.title="(Ő∀Ő3)ノ好哦！",st=setTimeout(function(){document.title=OriginTitile},3e3))});
+//rebuild by neat 
--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Sat Jun 20 2020 21:13:01 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
