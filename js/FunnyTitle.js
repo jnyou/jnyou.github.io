639ccb3a7b4a41f8362eb37945252291b9954bd7
jnyou <1447177020@qq.com>
2020-06-21 17:28:44 +0800

    Site updated: 2020-06-21 17:28:37

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,3 +1,3 @@
-// build time:Sun Jun 21 2020 17:19:45 GMT+0800 (GMT+08:00)
+// build time:Sun Jun 21 2020 17:28:12 GMT+0800 (GMT+08:00)
 var OriginTitile=document.title,st;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="ヽ(●-`Д´-)ノ你要玩捉迷藏嘛",clearTimeout(st)):(document.title="(Ő∀Ő3)ノ好哦！",st=setTimeout(function(){document.title=OriginTitile},3e3))});
 //rebuild by neat 
